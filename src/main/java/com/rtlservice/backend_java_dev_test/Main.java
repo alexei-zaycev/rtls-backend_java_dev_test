2648b724d51d9afa72034b407aafbb963bd4461b
Alexei Zaycev <alexei.zaycev@gmail.com>
2016-08-18 10:46:03 +0300

    fix unused import

--- src/main/java/com/rtlservice/backend_java_dev_test/Main.java
+++ src/main/java/com/rtlservice/backend_java_dev_test/Main.java
@@ -1,7 +1,6 @@
 package com.rtlservice.backend_java_dev_test;
 
 import java.util.Map;
-import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
